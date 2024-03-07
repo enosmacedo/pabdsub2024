94a6d8234e13d351c6cfa7db786e512ad0482dee
Daniel M <enosmacedo@gmail.com>
2024-03-07 14:08:40 -0300

    Modifiquei qualquer coisa so para mostrar

--- main.py
+++ main.py
@@ -78,8 +78,8 @@ def insert_element(conn, name_table, params):
     cmd_sql_colunas = "INSERT INTO " + name_table + "("
     cmd_sql_values  = " VALUES ("
 
-    for index, single_param in enumerate(params):
-        if index == len(params) - 1:
+    for index_vetor_params, single_param in enumerate(params):
+        if index_vetor_params == len(params) - 1:
             cmd_sql_colunas += single_param[0] + ") "
             cmd_sql_values += single_param[1] + ") "
         else:
@@ -99,11 +99,24 @@ def insert_element(conn, name_table, params):
         conn.commit()
         cursor.close()
 
+def update(conn, name_table, id, params):
+    daak;sdlkadsjkdasjijdasjkjkladskljjklads
+    dasflaksndfsdjklfndsfjklsdaf
+    asdfa
+    sdfasdfasdfasdf
+    pass
+
+
 if __name__ == '__main__':
     conn = connect_database("ticketcontroller", "postgres", "localhost", "postgres", 5432)
     # create_table(conn, "teste4", [("id", "SERIAL PRIMARY KEY"), ("rg", "INTEGER")  ])
 
     insert_element(conn, "teste4", [("id", "8"), ("rg", "12")])
+
+
+    update_element(conn, "teste4", "8", [("rg", "120")])
+
+
     # insert_element(conn, "teste4", [("id", "3"), ("rg", "116")])
     # insert_element(conn, "teste4", [("id", "4"), ("rg", "125")])
 
