0b6f2dd267654650fe6617451d7ee92585e786b4
edinhoparelhas <edinhoparelhasgmail.com>
2024-03-30 21:18:01 -0300

    criando os gets e sets das classes

--- Empresa.py
+++ Empresa.py
@@ -4,3 +4,27 @@ class Empresa:
         self.cnpj = cnpj
         self.email = email
         self.endereco = endereco
+
+    def getNome(self):
+        return self.nome
+
+    def getCNPJ(self):
+        return self.cnpj
+
+    def getEmail(self):
+        return self.email
+
+    def getEndereco(self):
+        return self.endereco
+    
+    def setNome(self, nome):
+        self.nome = nome
+
+    def setCNPJ(self, cnpj):
+        self.cnpj = cnpj
+
+    def setEmail(self, email):
+        self.email = email
+
+    def setEndereco(self, endereco):
+        self.endereco = endereco
